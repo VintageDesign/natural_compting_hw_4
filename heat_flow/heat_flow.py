783a76e343029e625ae4b8c55ddffa0cfcf8cb6c
Riley Kopp <koppril7@gmail.com>
2019-04-15 11:56:11 -0600

    Added the heatflow algorithm

--- heat_flow/heat_flow.py
+++ heat_flow/heat_flow.py
@@ -19,9 +19,16 @@ class HeatFlow:
 
     def updateFlow(self):
         new_grid = np.zeros(self.grid_size)
-        for i in range(1, self.grid_size(0)):
-            for j in range(0, self.grid_size(1)):
-                new_grid[i,j] = self.grid[i-1, j]
+
+        # Update side walls
+        for i in range(1, self.grid_size[0]-1-1):
+            new_grid[i,0] = self.grid[i-1, 0]
+            new_grid[i,self.grid_size[1]-1] = self.grid[i-1, self.grid_size[1]-1]
+            for j in range(1, self.grid_size[1]-2):
+                new_grid = round( (self.grid[i-1, j] + self.grid[i+1, j] + self.grid[i, j-1] + self.grid[i, j+1])/4)
+
+        self.grid = new_grid
+
 
     def showGrid(self):
         '''
@@ -34,8 +41,8 @@ class HeatFlow:
     def run(self):
         return_list = []
         for iteration in range(0, self.ticks):
-            self.update_flow()
-            if iteration % 100 = 0:
+            self.updateFlow()
+            if iteration % 100 == 0:
                 return_list.append(self.grid)
         return return_list
 
