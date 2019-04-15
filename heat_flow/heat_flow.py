e14ae3c75c8942a3921107ec482fd84d60cc04b0
Riley Kopp <koppril7@gmail.com>
2019-04-15 08:40:48 -0600

    Added the init for heatflow

--- heat_flow/heat_flow.py
+++ heat_flow/heat_flow.py
@@ -1,5 +1,5 @@
 import numpy as np
-from matplotlib.pyplot import imshow
+import pylab as plt
 
 class HeatFlow:
     def __init__(self, ticks, x_size, y_size):
@@ -13,14 +13,29 @@ class HeatFlow:
         self.grid_size = (x_size, y_size)
 
         self.grid = np.zeros(self.grid_size)
+        y = np.linspace(0,10, y_size)
+        self.grid[:, 0] = y * (y - 10)
 
-    def show_grid(self):
+
+    def updateFlow(self):
+        new_grid = np.zeros(self.grid_size)
+        for i in range(1, self.grid_size(0)):
+            for j in range(0, self.grid_size(1)):
+                new_grid[i,j] = self.grid[i-1, j]
+
+    def showGrid(self):
         '''
         Prints the grid
         '''
-        imshow(self.grid)
+        image = plt.imshow(self.grid, cmap='Greys')
+        plt.show()
 
 
     def run(self):
-        self.show_grid()
+        return_list = []
+        for iteration in range(0, self.ticks):
+            self.update_flow()
+            if iteration % 100 = 0:
+                return_list.append(self.grid)
+        return return_list
 
