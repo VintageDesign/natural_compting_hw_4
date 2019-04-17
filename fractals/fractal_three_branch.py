7cf4c2fcc4936b62a73e70c4a9e9b24c687764ca
Riley Kopp <riley.kopp@mines.sdsmt.edu>
2019-04-17 10:02:33 -0600

    Started adding color to the fractals

--- fractals/fractal_three_branch.py
+++ fractals/fractal_three_branch.py
@@ -1,21 +1,32 @@
 import turtle
+import random
 from time import sleep
 
 angle = 45
 
 
-def drawTree(level, size, T):
+def drawTree(level, size, T, color):
     """level = number of layers further to go"""
-    if level == 0 or size/1.5 == 0:
+    if level == 0:
         return
+
+    T.color(str(hex(color)))
+
+
+
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+
+    next_layer_color = r << 16 + g << 8 + b
 
     T.forward(size)
     T.left(angle)
-    drawTree(level - 1, size/1.5, T)
+    drawTree(level - 1, size/3, T, next_layer_color)
     T.right(2*angle)
-    drawTree(level - 1, size/1.5, T)
+    drawTree(level - 1, size/3, T, next_layer_color)
     T.left(angle)
-    drawTree(level - 1, size/1.5, T)
+    drawTree(level - 1, size/3, T, next_layer_color)
     T.backward(size)
 
 
@@ -28,8 +39,11 @@ def main():
     T.left(90)
     T.sety(-400)
     T.down()
-    drawTree(5, 300, T)
-    sleep(100)
+    drawTree(5, 300, T, int(FFFFFF))
+    ts = turtle.getscreen()
+
+    ts.getcanvas().postscript(file="fractal.eps")
+    turtle.clearscreen()
 
 
 if __name__ == '__main__':
